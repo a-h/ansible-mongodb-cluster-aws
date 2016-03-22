e9ced6f87bfb6a2e974f66d59e92d91a46fcbe02
Adrian Hesketh <a-h@users.noreply.github.com>
2016-03-22 16:56:41 +0000

    Updated installation for Amazon Linux.

--- 01_create_cluster.sh
+++ 01_create_cluster.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i hosts 01_create_cluster.yml -u ec2-user --private-key=../private_key.pem 
+ansible-playbook -i hosts 01_create_cluster.yml -u ec2-user --private-key=private_key.pem 
